6e55ef7f075a54ae695fc680901914ab484acc6f
samuelcarlson18 <99274631+samuelcarlson18@users.noreply.github.com>
2022-05-11 00:33:37 +0000

    code

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// Copyright (c) 2022 Samuel Carlson All rights reserved
+//
+// Created by: Samuel Carlson
+// Created on: April 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+
+
+/**
+ * This function displays the users total based on what they ordered
+ */
+function displayAnswer () {
+  
+  	// initialize variables (answer)
+	let answer = ""
+	
+	// get user input (what size they would like)
+	let size = parseInt(document.getElementById('size').value)
+
+  // get user input (what size they would like)
+	let sauce = parseInt(document.getElementById('sauce').value)
+
+  // get user input (would they like a drink)
+	let drink = parseInt(document.getElementById('drink').value)
+  
+   // determine the answer based on what size and sauce the user chose and whether or not they want a drink
+
+    if () {
+
+        answer = ""
+
+    }
+
+    else if () {
+
+        answer = ""
+
+    }  
+
+	else if () {
+
+        answer = ""
+    }
+
+  else {
+
+        answer = ""
+
+    }
+
+
+  // display the results 
+  document.getElementById('answer').innerHTML = answer
+
+}
